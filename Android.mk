7c0d67243b669dbe3411e681b8593d816ba225ed
Zeeshsan AJ <itexpert120@gmail.com>
2020-03-29 18:53:06 +0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter fortuna3g,$(TARGET_DEVICE)),)
+ifneq ($(filter fortuna3g fortunave3g,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
